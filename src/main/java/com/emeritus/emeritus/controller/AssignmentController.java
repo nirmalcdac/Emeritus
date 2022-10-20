24d58f41c40c89c3b86fba687d8c6f691c9efbed
nirmalsharma <nirmal.sharma3500@gmail.com>
2022-10-20 12:03:33 +0530

    URL optimisation

--- src/main/java/com/emeritus/emeritus/controller/AssignmentController.java
+++ src/main/java/com/emeritus/emeritus/controller/AssignmentController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/assignment")
 public class AssignmentController {
     @Autowired
     private final AssignmentServiceImpl assignmentService;
